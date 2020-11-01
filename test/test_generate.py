c80d1d50f066cb5742648922bc715e2c28e06d52
Patrick Shriwise <pshriwise@gmail.com>
2020-11-01 17:15:22 -0600

    More checks for property updates.

--- test/test_generate.py
+++ test/test_generate.py
@@ -1,12 +1,26 @@
 
+import numpy as np
+from numpy.testing import assert_array_equal
+
 from py_iga.particle_gen import Particle, ParticleGenerator
+
 
 def test_parrticle():
 
     p = Particle()
 
-    assert p.r == (0.0, 0.0, 0.0)
-    assert p.u == (1.0, 0.0, 0.0)
+    assert_array_equal(p.r, (0.0, 0.0, 0.0))
+    assert_array_equal(p.u, (1.0, 0.0, 0.0))
+    assert p.e == 10.0
+
+    p.r = (5.0, 0.0, 5.0)
+    p.u = (0.7071, 0.0, 0.7071)
+    p.e = 0.1
+
+    assert_array_equal(p.r, (5.0, 0.0, 5.0))
+    assert_array_equal(p.u, (0.7071, 0.0, 0.7071))
+    assert p.e == 0.1
+
 
 def test_generate():
 
@@ -14,5 +28,5 @@ def test_generate():
 
     p = gen()
 
-    assert p.r == (0.0, 0.0, 0.0)
-    assert p.u != (1.0, 0.0, 0.0)
+    assert_array_equal(p.r, (0.0, 0.0, 0.0))
+    assert not np.array_equal(p.u, (1.0, 0.0, 0.0))
