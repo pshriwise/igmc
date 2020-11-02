4eea9ba5715c571582cd6aec33136be3ab4c6219
Patrick Shriwise <pshriwise@gmail.com>
2020-11-01 20:17:16 -0600

    Adding callable distributions for space, angle, energy in the particle generator.

--- py_iga/particle_gen.py
+++ py_iga/particle_gen.py
@@ -1,23 +1,60 @@
+from collections import Callable
+
 import numpy as np
 from numpy.random import rand
 
-
 from .particle import Particle
+from .distributions import isotropic_dir
+from . import checkvalue as cv
 
 class ParticleGenerator:
 
+    def __init__(self, space=None, angle=None, energy=None):
+        self.space = space if space else self._default_spatial
+        self.angle = angle if angle else self._default_angle
+        self.energy = energy if energy else self._default_engery
+
     def __call__(self):
-        # all particles start on the origin for now
-        xyz = (0.0, 0.0, 0.0)
+        xyz = self.space()
+        uvw = self.angle()
+        energy = self.energy()
+        return Particle(r=xyz, u=uvw, e=energy)
 
-        # isotropic sampling in direction
-        phi = rand() * 2.0 * np.pi
-        t = -1.0 + 2.0 * rand()
+    @property
+    def space(self):
+        return self._space
 
-        u = np.sqrt(1 - t*t) * np.cos(phi)
-        v = np.sqrt(1 - t*t) * np.sin(phi)
-        w = t
+    @space.setter
+    def space(self, val):
+        cv.check_type('spatial distribution', val, Callable)
+        self._space = val
 
-        uvw = (u, v, w)
+    @property
+    def angle(self):
+        return self._angle
 
-        return Particle(r=xyz, u=uvw)
+    @angle.setter
+    def angle(self, val):
+        cv.check_type('angular distribution', val, Callable)
+        self._angle = val
+
+    @property
+    def energy(self):
+        return self._energy
+
+    @energy.setter
+    def energy(self, val):
+        cv.check_type('energy distribution', val, Callable)
+        self._energy = val
+
+    @staticmethod
+    def _default_spatial():
+        return (0.0, 0.0, 0.0)
+
+    @staticmethod
+    def _default_angle():
+        return isotropic_dir()
+
+    @staticmethod
+    def _default_engery():
+        return 10.0
