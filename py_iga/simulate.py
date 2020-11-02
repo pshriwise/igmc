9196cc2765f80568d082b0f2c8a10ee4959a109d
Patrick Shriwise <pshriwise@gmail.com>
2020-11-01 22:55:54 -0600

    Adding simple simulation script.

---
+++ py_iga/simulate.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.random import rand
+from particle_gen import ParticleGenerator
+
+def simulate():
+
+    ## Temporary Parameter Storage ##
+    n_particles = 1000
+    e_min = 1E-03
+    majorant_xs = 10
+    total_xs = 9.9
+    seed = 110
+
+    np.random.seed(seed)
+
+    particle_generator = ParticleGenerator()
+
+    for i in range(n_particles):
+        p = particle_generator()
+
+        while p.e > e_min:
+            p.advance(majorant_xs)
+
+            if rand() < total_xs / majorant_xs:
+                p.scatter()
+
+        print(p)
+
+if __name__ == "__main__":
+    simulate()
