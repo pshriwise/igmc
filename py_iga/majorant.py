f5bb7ecc403cc4bffda96727702c4c911faad0b7
Patrick Shriwise <pshriwise@gmail.com>
2020-11-17 23:51:44 -0600

    Removing the simple plot example from the majorant module.

--- py_iga/majorant.py
+++ py_iga/majorant.py
@@ -6,7 +6,6 @@ import sys
 from .binary_search import binary_search
 from . import checkvalue as cv
 
-from matplotlib import pyplot as plt
 import numpy as np
 import openmc
 from openmc.plotter import calculate_cexs
@@ -554,23 +553,3 @@ class Majorant(Max2D):
             majorant.update(energy_grid, other.xs(energy_grid))
 
         return majorant
-
-
-if __name__ == "__main__":
-    e_1 = (1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0)
-    xs_1 = (1.0, 1.0, 3.0, 3.0, 1.0, 3.0, 3.0)
-
-    e_2 = (1.25, 1.5, 1.75, 2.75, 2.9, 3.5, 4.0, 5.5, 6.5, 7.0)
-    xs_2 = (0.0, 0.0, 2.0, 2.0, 4.0, 4.0, 2.0, 2.0, 2.0, 3.0)
-
-    majorant = Max2D()
-    majorant.update(e_1, xs_1)
-    majorant.update(e_2, xs_2)
-
-    plt.plot(e_1, xs_1, marker='.')
-    plt.plot(e_2, xs_2, marker='.')
-    plt.show() # plot xs's only
-    plt.plot(e_1, xs_1, marker='.')
-    plt.plot(e_2, xs_2, marker='.')
-    plt.plot(majorant.x_values, majorant.y_values, marker='.')
-    plt.show() # plot w/ majorant
